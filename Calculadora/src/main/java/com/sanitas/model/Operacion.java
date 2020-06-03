64e1eea6b552d2f66ce6a6a9856c0297fb62786a
curro <curro@192.168.1.35>
2020-06-03 17:13:03 +0200

    Se añade enum para gestionar las diversas operaciones Se añade la dependencia de lombok para hacer el código más claro

---
+++ Calculadora/src/main/java/com/sanitas/model/Operacion.java
@@ -0,0 +1,40 @@
+package com.sanitas.model;
+
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import lombok.AllArgsConstructor;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public enum Operacion {
+	SUMA("+"),
+    RESTA("-");
+	
+	//TODO añadir más operaciones
+	
+	//La operación podrá ser representada o bién por el literal o bién por el signo matemático
+	public String getSigno() {
+		return signo;
+	}
+
+	private static final Operacion[] valores = new Operacion[]{SUMA, RESTA};
+	
+	private String signo;
+	
+	@JsonCreator
+    public static Operacion desdeValor(String valor) {
+
+        for (int i = 0; i < valores.length; ++i) {
+            Operacion opActual = valores[i];
+            if (valor.equalsIgnoreCase(opActual.name()) ||
+                    valor.equalsIgnoreCase(opActual.getSigno())) {
+                return opActual;
+            }
+        }
+
+        throw new RuntimeException("Operación no soportada para el valor: " + valor);
+    }
+}
