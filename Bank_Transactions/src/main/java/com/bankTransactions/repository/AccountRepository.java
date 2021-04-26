6111db0293f24ce0cc39d72149951ba057c12fa0
Azorruc <josemazorin@yahoo.es>
2021-04-26 16:07:59 +0200

    Delete Bank_Transactions directory

--- Bank_Transactions/src/main/java/com/bankTransactions/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bankTransactions.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bankTransactions.model.Account;
-
-public interface AccountRepository extends JpaRepository<Account, String> {
-
-}
